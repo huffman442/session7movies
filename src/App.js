ca7b1b340ee2aca6eb81f8d783176a0982312df0
John Lange <lange.john.m@gmail.com>
2018-10-10 14:07:12 -0400

    Updated api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import data from './apidata.js';
+import Axios from 'axios';
 
 class App extends Component {
   constructor(props)
@@ -9,13 +8,33 @@ class App extends Component {
     super(props)
 
     this.state = {
-      moviedata: data
+      moviedata: {
+        results: [],
+      }
     }
+
+    Axios
+      .get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6fbc7f3f313bd395902af464ef47262")
+      .then((response) => {
+        this.setState({moviedata: response.data});
+      })
+
+    this.gotoPageTwo = this.gotoPageTwo.bind(this)
   }
-  
+
+  gotoPageTwo()
+  {
+    Axios
+    .get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6fbc7f3f313bd395902af464ef47262&page=2")
+    .then((response) => {
+      this.setState({moviedata: response.data});
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <h2 onClick={this.gotoPageTwo}>Load them movies!</h2>
         {this.state.moviedata.results.map( (movie) => {
           return (
            <div>
